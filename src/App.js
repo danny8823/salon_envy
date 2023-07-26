61cee0ba2a0a1e1702f0ee3b2df2b68df8c8a89b
danny8823 <dannyyoo714@gmail.com>
2023-07-26 08:54:49 -0700

    added framer motion / styled-components module, styles and images

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {Header, BackgroundImg, Nav, Img, Paragraph} from './style'
+import bannerimg from './images/banner.jpg'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header>
+        <BackgroundImg src = {bannerimg} alt ="banner image"/>
+        <Nav>
+          <a href = '#'>Home</a>
+          <a href = '#'>About</a>
+          <a href = '#'>Contact</a>
+        </Nav>
+        <Paragraph>
+          Salon Envy
+        </Paragraph>
+      </Header>
+    </>
   );
 }
 
