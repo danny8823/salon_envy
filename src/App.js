206bcfd78231d1db03408d37caf39593986f5f03
danny8823 <dannyyoo714@gmail.com>
2023-07-26 10:46:27 -0700

    Finished weblayout, added images and texts, about and contact section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import {Header, BackgroundImg, Nav, Img, Paragraph} from './style'
+import { useScroll, useTransform } from "framer-motion"
+import {Header, BackgroundImg, Nav, Img, Paragraph, Title, Section, AboutSection, AboutTitle, AboutParagraph, ContactSection, ContactParagraph, ContactTitle, Icons, IconText, Footer, HeaderTitle} from './style'
 import bannerimg from './images/banner.jpg'
+import hair1 from './images/hairdone.jpg'
+import model1 from './images/model1.jpg'
+import model2 from './images/model2.jpg'
+import hair2 from './images/hairdone2.jpg'
+import model3 from './images/model3.jpg'
+import wedding from './images/wedding.jpeg'
+import team from './images/team.jpg'
+import phone from './images/phone.png'
+import mail from './images/mail.png'
+import yelp from './images/yelp.png'
 
 const App = () => {
+
+  const { scrollYProgress } = useScroll()
+  const x = useTransform(scrollYProgress, [0, 1], [0,800])
+  const y = useTransform(scrollYProgress, [0, 1], [0, 300])
+
   return (
     <>
       <Header>
@@ -11,10 +27,63 @@ const App = () => {
           <a href = '#'>About</a>
           <a href = '#'>Contact</a>
         </Nav>
-        <Paragraph>
-          Salon Envy
-        </Paragraph>
+        <HeaderTitle style={{x}}>
+        From Ordinary to Extraordinary: Elevate Your Image with Us
+        </HeaderTitle>
+        <Title style={{x}}>Salon Envy, your next salon.</Title>
       </Header>
+      <Section>
+        <Img src = {hair1} alt = 'woman getting hair done'/>
+        <Paragraph style ={{y}}>
+          "At Salon Envy our dedicated hairdressers turn your hair into a work of art, with years of experience we put great care and dedication to service you. We guarantee you will love your hair."
+        </Paragraph>
+      </Section>
+      <Section>
+        <Paragraph style ={{y}}>
+          "Modeling to just getting your hair done for a night out"
+        </Paragraph>
+        <Img src = {model1} alt = 'model with purple hair'/>
+      </Section>
+      <Section>
+        <Img src = {model2} alt = 'model'/>
+        <Paragraph style ={{y}}>Modeling styles</Paragraph>
+      </Section>
+      <Section>
+        <Paragraph style ={{y}}>Check it out</Paragraph>
+        <Img src = {hair2} alt = 'woman getting hair done'/>
+      </Section>
+      <Section>
+        <Img src = {model3} alt = 'model'/>
+        <Paragraph style ={{y}}>Styling for photoshoots</Paragraph>
+      </Section>
+      <Section>
+        <Paragraph style ={{y}}>For weddings</Paragraph>
+        <Img src = {wedding} alt = 'wedding photo'/>
+      </Section>
+      <AboutSection>
+        <AboutTitle>About us</AboutTitle>
+        <Img src = {team} alt = 'team of hairdressers'/>
+        <AboutParagraph>
+          We are a team of dedicated hairdressers, working hard to bring out the best from your hair.
+        </AboutParagraph>
+      </AboutSection>
+      <ContactSection>
+        <ContactTitle>
+          Contact
+        </ContactTitle>
+        <ContactParagraph>
+          Give us a call and set your appointment today!
+        </ContactParagraph>
+        <IconText><Icons src = {phone} alt = 'phone logo'/> 777-100-2000</IconText>
+        <IconText><Icons src = {mail} alt = 'email logo'/>EnvySalon@gmail.com</IconText>
+        <IconText><Icons src = {yelp} alt = 'yelp logo'/><a href = '#'>Envy Salon</a></IconText>
+      </ContactSection>
+      <Footer>
+        <small>Trademark of Envy Salons Inc.</small>
+        <a href = '#'>Home</a>
+        <a href = '#'>Contact</a>
+        <a href = '#'>About</a>
+      </Footer>
     </>
   );
 }
--- src/style.js
+++ src/style.js
@@ -11,6 +11,17 @@ export const Header = styled.div`
   color: white;
 `;
 
+export const HeaderTitle = styled(motion.p)`
+  max-width: 900px;
+  font-size: 30px;
+  position: relative;
+  z-index: 1;
+  margin: 17vh;
+  transition: all 2s ease-in-out;
+  color: black;
+  bottom: 50px;
+  font-weight: bold;
+`
 export const BackgroundImg = styled.img`
   width: 100%;
   height: 100%;
@@ -20,14 +31,13 @@ export const BackgroundImg = styled.img`
 `;
 
 export const Title = styled(motion.h1)`
-  font-family: var(--font-primary);
   color: white;
-  font-size: 20em;
+  font-size: 8em;
   z-index: 1;
   margin: 0;
   white-space: nowrap;
   margin-top: auto;
-  transition: all 2s ease-in-out;
+  transition: all 1s ease-in-out;
 `;
 
 export const Nav = styled.nav`
@@ -45,19 +55,23 @@ export const Nav = styled.nav`
   }
 `;
 
-export const Paragraph = styled.p`
-  max-width: 900px;
-  font-size: 32px;
+export const Paragraph = styled(motion.p)`
+  max-width: 500px;
+  font-size: 24px;
   position: relative;
+  z-index: 1;
   margin: 17vh;
+  transition: all 2s ease-in-out;
 `;
 
 export const Img = styled.img`
-  width: 25em;
+  width: 40em;
+  box-shadow: 2px 2px 10px white;
 `;
 
 export const Section = styled.div`
   height: 80vh;
+  background-color: gray;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -66,3 +80,99 @@ export const Section = styled.div`
   padding: 10em 0;
   color: white;
 `;
+
+export const AboutSection = styled.div`
+  height: 90vh;
+  background-color: gray;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  overflow: hidden;
+  padding: 10em 0;
+  color: white;
+`
+export const AboutTitle = styled.h1`
+  color: white;
+  font-size: 8em;
+  z-index: 1;
+  margin: 40px;
+  white-space: nowrap;
+  margin-top: auto;
+  border-bottom-style: solid;
+  border-bottom-color: black;
+  border-bottom-width: 2px;
+  border-bottom-length: 60px;
+`;
+
+export const AboutParagraph = styled.p`
+  max-width: 500px;
+  font-size: 24px;
+  position: relative;
+  margin: 10vh;
+`;
+
+export const ContactSection = styled.div`
+  height: 40vh;
+  background-color: gray;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  overflow: hidden;
+  padding: 10em 0;
+  color: white;
+`;
+
+export const ContactParagraph = styled.p`
+  max-width: 500px;
+  font-size: 24px;
+  position: relative;
+  margin: 10vh;
+`;
+
+export const ContactTitle = styled.h1`
+  color: white;
+  font-size: 8em;
+  z-index: 1;
+  margin: 20px;
+  white-space: nowrap;
+  margin-top: auto;
+  border-bottom-style: solid;
+  border-bottom-color: black;
+  border-bottom-width: 2px;
+  border-bottom-length: 60px;
+`;
+
+export const IconText = styled.p`
+  font-size:26px;
+  margin: 10px;
+`
+export const Icons = styled.img`
+  width: 20px;
+`;
+
+export const Footer = styled.nav`
+  width: 100%;
+  height: 20vh;
+  background-color: black;
+  display: flex;
+  small {
+    display: flex;
+    color: white;
+    align-items: flex-end;
+  }
+  a {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    color: white;
+    width: 50%;
+    text-decoration: none;
+    font-size: 20px;
+    &:hover {
+      color: green;
+    }
+`
